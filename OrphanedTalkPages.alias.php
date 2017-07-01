165520934237766eb2cdbef50704f265993376a2
MarcoAurelio <maurelio@tools.wmflabs.org>
2017-07-01 14:27:28 +0000

    Add special page alias in Spanish language
    Change-Id: Iec06c7044c3f7d2b73b67e8429a3d39ff408b633

--- OrphanedTalkPages.alias.php
+++ OrphanedTalkPages.alias.php
@@ -19,3 +19,8 @@ $specialPageAliases['en'] = [
 $specialPageAliases['nl'] = [
 	'OrphanedTalkPages' => [ 'WeesOverlegPaginas' ],
 ];
+
+/** Spanish (español) */
+$specialPageAliases['es'] = [
+	'OrphanedTalkPages' => [ 'Páginas_de_discusión_huérfanas', 'Discusiones_huérfanas' ],
+];
