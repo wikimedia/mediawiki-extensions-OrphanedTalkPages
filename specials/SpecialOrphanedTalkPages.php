5c8d63bd9871c354135cad5f93817453a67f3a3c
mainframe98 <k.s.werf@hotmail.com>
2018-10-08 10:16:33 +0200

    Add MediaWiki Code Sniffer and get the sniffs to pass
    Change-Id: I3cc2d604520d860cdd9c0c00ea51e0eda68ddb1b

--- specials/SpecialOrphanedTalkPages.php
+++ specials/SpecialOrphanedTalkPages.php
@@ -1,33 +1,74 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: kswer
- * Date: 26-2-2017
- * Time: 19:37
- */
 class SpecialOrphanedTalkPages extends PageQueryPage {
-	function __construct( $name = 'OrphanedTalkPages' ) {
-		parent::__construct( $name );
+	public function __construct() {
+		parent::__construct( 'OrphanedTalkPages' );
 	}
 
-	function getPageHeader() {
+	/**
+	 * The content returned by this function is output before any result.
+	 *
+	 * @return string
+	 */
+	public function getPageHeader() {
 		return $this->msg( 'orphanedtalkpages-text' )->parseAsBlock();
 	}
 
-	function sortDescending() {
+	/**
+	 * Overridden to prevent sorting by increasing values.
+	 *
+	 * @return bool
+	 */
+	public function sortDescending() {
 		return false;
 	}
 
+	/**
+	 * Is this query expensive? Then we
+	 * don't let it run in miser mode. $wgDisableQueryPages causes all query
+	 * pages to be declared expensive. Some query pages are always expensive.
+	 *
+	 * @return bool
+	 */
 	public function isExpensive() {
 		return true;
 	}
 
-	function isSyndicated() {
+	/**
+	 * Sometime we don't want to build rss / atom feeds.
+	 *
+	 * @return bool
+	 */
+	public function isSyndicated() {
 		return false;
 	}
 
-	public function getQueryInfo() {
+	/**
+	 * Subclasses return an SQL query here, formatted as an array with the
+	 * following keys:
+	 *    tables => Table(s) for passing to Database::select()
+	 *    fields => Field(s) for passing to Database::select(), may be *
+	 *    conds => WHERE conditions
+	 *    options => options
+	 *    join_conds => JOIN conditions
+	 *
+	 * Note that the query itself should return the following three columns:
+	 * 'namespace', 'title', and 'value'. 'value' is used for sorting.
+	 *
+	 * These may be stored in the querycache table for expensive queries,
+	 * and that cached data will be returned sometimes, so the presence of
+	 * extra fields can't be relied upon. The cached 'value' column will be
+	 * an integer; non-numeric values are useful only for sorting the
+	 * initial query (except if they're timestamps, see usesTimestamps()).
+	 *
+	 * Don't include an ORDER or LIMIT clause, they will be added.
+	 *
+	 * If this function is not overridden or returns something other than
+	 * an array, getSQL() will be used instead. This is for backwards
+	 * compatibility only and is strongly deprecated.
+	 * @return array
+	 */
+	public function getQueryInfo() : array {
 		global $wgOrphanedTalkPagesExemptedNamespaces, $wgOrphanedTalkPagesIgnoreUserTalk;
 
 		$exemptedNamespaces = [];
@@ -49,7 +90,8 @@ class SpecialOrphanedTalkPages extends PageQueryPage {
 			'fields' => [
 				'namespace' => 'p1.page_namespace',
 				'title' => 'p1.page_title',
-			    'value' => 'page_title' // Sorting
+				// Sorting
+				'value' => 'page_title'
 			],
 			'conds' => [
 				'p1.page_title NOT LIKE "%/%"',
@@ -72,6 +114,11 @@ class SpecialOrphanedTalkPages extends PageQueryPage {
 		return $query;
 	}
 
+	/**
+	 * Under which header this special page is listed in Special:SpecialPages.
+	 *
+	 * @return string
+	 */
 	protected function getGroupName() {
 		return 'maintenance';
 	}
